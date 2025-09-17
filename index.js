4be7e2e2e7758410deabd159eb1215c1143942be
Isaac Rideal <isaac.rideal@gmail.com>
2025-09-17 17:44:53 +0100

    Wrote function logic

--- index.js
+++ index.js
@@ -4,5 +4,14 @@ let number_of_years = 30
 let interest_rate = 10
 
 const compoundInterest = (init_amount, monthly_contribution, number_of_years, interest_rate) => {
+    let total = init_amount
 
+    let annual_contribtuion = monthly_contribution * 12
+
+    for (let i = 0; i < number_of_years; i++) {
+        total = total + annual_contribtuion
+        total = total * (100 + interest_rate / 100)
+    }
+
+    return total
 }
