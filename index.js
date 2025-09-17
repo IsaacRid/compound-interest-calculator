f7344ed9cf5e6506b4d5f9e56acaba0de427551e
Isaac Rideal <isaac.rideal@gmail.com>
2025-09-17 17:39:26 +0100

    Initialised values and wrote function definition

---
+++ index.js
@@ -0,0 +1,8 @@
+let init_amount = 20000
+let monthly_contribution = 400
+let number_of_years = 30
+let interest_rate = 10
+
+const compoundInterest = (init_amount, monthly_contribution, number_of_years, interest_rate) => {
+
+}
