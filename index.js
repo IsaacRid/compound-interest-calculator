f1a73e262c5700c8775ba33bd046bc65362aac5d
Isaac Rideal <isaac.rideal@gmail.com>
2025-09-18 14:29:14 +0100

    Wrote function to print all inputs and outputs of functions using string literals

--- index.js
+++ index.js
@@ -19,3 +19,14 @@ const compoundInterest = (init_amount, monthly_contribution, number_of_years, in
 const regularSaving = (init_amount, monthly_contribution, number_of_years) => {
     return (init_amount + monthly_contribution * 12 * number_of_years).toFixed(2)
 }
+
+
+const printOutput = (init_amount, monthly_contribution, number_of_years, interest_rate) => {
+    let final_value = compoundInterest(init_amount, monthly_contribution, number_of_years, interest_rate)
+    let value_without_compounding = regularSaving(init_amount, monthly_contribution, number_of_years)
+    let summary = `INITIAL_AMOUNT: £${init_amount}\nMONTHLY_CONTRIBUTION: £${monthly_contribution}\nNUMBER_OF_YEARS: ${number_of_years}\nINTEREST_RATE: ${interest_rate}%\n\n
+    FINAL_COMPOUNDED_VALUE: £${final_value}\n
+    REGULAR_SAVING_VALUE: £${value_without_compounding}\n
+    VALUE_DIFFERENCE: £${final_value - value_without_compounding}`
+    console.log(summary)
+}
